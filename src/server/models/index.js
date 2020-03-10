db47d8a150763a2e92f8a6766a9715a7defa1917
Igsu <juan.bravo@futurice.com>
2020-03-10 19:21:46 +0200

    removed compiled files

--- src/server/models/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var events_1 = require("events");
-exports.eventsModel = events_1["default"];
--- src/server/types.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var EApiPath;
-(function (EApiPath) {
-    EApiPath["eventsPath"] = "api/v1/event";
-})(EApiPath || (EApiPath = {}));
-exports.EApiPath = EApiPath;
