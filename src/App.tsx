5673ff419d38f8e88174b1b29a6aeb14b25cafbc
Igsu-Bravo <juan.bravo@futurice.com>
2021-05-13 13:29:39 +0300

    Improve layout + add form

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,24 @@
 import React from 'react';
 import Header from './components/Header';
+import SongForm from './components/SongForm';
+import Wave from './assets/Wave';
 
 const App = (): React.ReactElement => {
   return (
     <div className="bg-main min-h-screen">
       <Header title="J u p i t e r"></Header>
-      <main className="bg-white rounded-sm m-5">
-        <div className="container mx-auto bg-grayLight rounded-sm m-3">
-          <p>form here</p>
+      <main className="bg-grayLight rounded-sm m-5 py-3 ">
+        <div className="flex flex-wrap">
+          <div className="flex-auto bg-white rounded-sm m-3 p-5">
+            <SongForm />
+          </div>
+          <div className="flex-auto rounded-sm m-3 p-5">
+            <div className="justify-self-center">
+              <Wave />
+            </div>
+          </div>
         </div>
-        <div className="container mx-auto  bg-grayLight rounded-sm m-3">
+        <div className="flex flex-wrap bg-white rounded-sm m-3">
           <ul className="list-disc m-2">
             <li>Una vez</li>
             <li>Le prregunte a mi gato</li>
